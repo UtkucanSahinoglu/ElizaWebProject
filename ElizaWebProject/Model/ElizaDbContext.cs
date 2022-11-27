a1e70d06f474426100124556bf6ed65afb4e6c84
UtkucanSahinoglu <utkucannsahinoglu@gmail.com>
2022-11-27 15:09:26 +0300

    Add project files.

---
+++ ElizaWebProject/Model/ElizaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ElizaWebProject.Model
+{
+    public class ElizaDbContext : IdentityDbContext
+    {
+        public ElizaDbContext(DbContextOptions<ElizaDbContext> options) : base(options)
+        {
+
+        }
+    }
+
+}
